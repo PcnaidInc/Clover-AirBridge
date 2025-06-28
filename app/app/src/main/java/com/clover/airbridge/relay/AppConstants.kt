ffe5b456a91a5fd4803cb77d0318076c5fdb8898
Pcnaid Inc <Support@pcnaid.com>
2025-06-28 17:57:42 -0500

    Create AppConstants.kt

---
+++ app/app/src/main/java/com/clover/airbridge/relay/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.clover.airbridge.relay
+
+object AppConstants {
+    // This RAID must be generated from your Clover Developer Dashboard
+    const val REMOTE_APP_ID = "YOUR_REMOTE_APP_ID_HERE"
+    const val FLEX_IP_ADDRESS = "192.168.1.105" // Example: This should be configurable
+    const val FLEX_PORT = 12345 // Example: This should be configurable
+}
